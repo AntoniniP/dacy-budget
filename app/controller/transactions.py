39d5e1b70cd7cc05ba1a79c68846c3a783a90d27
Paolo Antonini <paolo1.antonini@mail.polimi.it>
2021-01-06 11:58:18 +0000

    Fixes

--- app/controller/transactions.py
+++ app/controller/transactions.py
@@ -3,18 +3,30 @@ import flask_login
 
 def new_transaction():
     from app import db
-    from ..models.transaction_details import TransactionDetail
-    from ..models.transaction_headers import TransactionHeader  
+    from app.models.transaction_headers import TransactionHeader
 
     try:
         current_user = flask_login.current_user.id
         th = TransactionHeader(user=current_user)
         db.session.add(th)
 
+        _insert_details(th)
+
+    except IntegrityError as e:
+        print(e)
+        db.session.rollback()
+    else:
+        db.session.commit()
+
+
+def _insert_details(th):
+    from app import db
+    from app.models.transaction_details import TransactionDetail
+    
+    try:
         # transaction = Transaction(id=(datetime.now()), account=account, date=date, narration=narration, amount=amount)
         td = TransactionDetail(header=th)
         db.session.add(td)
-
     except IntegrityError as e:
         print(e)
         db.session.rollback()
--- app/models/categories.py
+++ app/models/categories.py
@@ -6,5 +6,7 @@ class Category(db.Model):
 
     id = db.Column(db.BigInteger, primary_key=True)
     description = db.Column(db.String(255))
+    # user
 
     # details = db.relationship("TransactionDetail", back_populates="transaction_headers")
+    
